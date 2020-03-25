84fb295f96106cea6c3e165a18cef19fc8816e9b
mahamani140 <60865894+mahamani140@users.noreply.github.com>
2020-03-25 14:54:03 +0530

    Update UpdateProfile_Crossword_Page.java

--- BooksWagon_Website/src/test/java/com/Crossword/Pages/UpdateProfile_Crossword_Page.java
+++ BooksWagon_Website/src/test/java/com/Crossword/Pages/UpdateProfile_Crossword_Page.java
@@ -24,38 +24,38 @@ public class UpdateProfile_Crossword_Page
 	//To Launch Browser
 	public void LaunchBrowser() throws IOException 
 	{ 
-		System.setProperty("webdriver.chrome.driver","src\\test\\resources\\Driver\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-		driver.get("https://www.crossword.in/");
-		System.out.println(driver.getTitle());
+		System.setProperty("webdriver.chrome.driver","src\\test\\resources\\Driver\\chromedriver.exe");	//Set path to chromedriver.exe
+		driver = new ChromeDriver();	//create chrome instance
+		driver.manage().window().maximize();	//To Maximize the window
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);	//Time to wait
+		driver.get("https://www.crossword.in/");	//Get the crossword url
+		System.out.println(driver.getTitle());	//To get the title
 		
-		Crossword_Excel data = new Crossword_Excel();
-		driver.findElement(By.linkText("Login")).click();
-		driver.findElement(By.id("user_session_email")).sendKeys(data.excel_username(1)); 
-		driver.findElement(By.id("user_session_password")).sendKeys(data.excel_password(1));
-		driver.findElement(By.xpath("//*[@id=\"user_session_submit\"]")).click();
+		Crossword_Excel data = new Crossword_Excel();	//Create an object for excel 
+		driver.findElement(By.linkText("Login")).click();	//Find Login Button
+		driver.findElement(By.id("user_session_email")).sendKeys(data.excel_username(1)); //Find username and send username by excel sheet
+		driver.findElement(By.id("user_session_password")).sendKeys(data.excel_password(1));	//Find password and send password by excel sheet
+		driver.findElement(By.xpath("//*[@id=\"user_session_submit\"]")).click();	//Click the submit Button
 	}
 	
 	//To Update Profile
 	public void updateProfile(int a) throws IOException, InterruptedException
 	{
-		Crossword_Excel data = new Crossword_Excel();
+		Crossword_Excel data = new Crossword_Excel();	//Create an object for excel file
 		Thread.sleep(1000);
-		driver.findElement(myAccount).click();
+		driver.findElement(myAccount).click();	//Find and click the myaccount option
 		Thread.sleep(3000);
-		driver.findElement(updateProfile).click();
-		driver.findElement(mobile).sendKeys(data.excel_mobile(a));
-		driver.findElement(landLine).sendKeys(data.excel_landline(a));
-		driver.findElement(updateButton).click();
+		driver.findElement(updateProfile).click();	//Find and click the update profile option
+		driver.findElement(mobile).sendKeys(data.excel_mobile(a));	//Find the mobile element and send mobile number by excel file
+		driver.findElement(landLine).sendKeys(data.excel_landline(a));	//Find the landline element and send landline number by excel file
+		driver.findElement(updateButton).click();	//Find and click the update button
 	}
 	
 	public void close_updateProfile() throws InterruptedException
 	{
-		driver.findElement(By.linkText("Logout")).click();
+		driver.findElement(By.linkText("Logout")).click(); //Find and click the logout
 		Thread.sleep(2000);
-		driver.close();
+		driver.close();	//To close the browser
 	}
 	
 }
