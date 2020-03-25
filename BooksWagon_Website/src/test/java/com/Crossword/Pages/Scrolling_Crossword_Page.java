7c43d361235fe5d00c26a6a9b35b3048a4336086
mahamani140 <60865894+mahamani140@users.noreply.github.com>
2020-03-25 14:47:20 +0530

    Update Scrolling_Crossword_Page.java

--- BooksWagon_Website/src/test/java/com/Crossword/Pages/Scrolling_Crossword_Page.java
+++ BooksWagon_Website/src/test/java/com/Crossword/Pages/Scrolling_Crossword_Page.java
@@ -21,41 +21,41 @@ public class Scrolling_Crossword_Page
 	//To Launch Browser
 	public void LaunchBrowser() throws IOException 
 	{ 
-		System.setProperty("webdriver.chrome.driver","src\\test\\resources\\Driver\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-		driver.get("https://www.crossword.in/");
-		System.out.println(driver.getTitle());
+		System.setProperty("webdriver.chrome.driver","src\\test\\resources\\Driver\\chromedriver.exe");	//Set path to chromedriver.exe
+		driver = new ChromeDriver();	//create chrome instance
+		driver.manage().window().maximize();	//To Maximize the window
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);	//Time to wait
+		driver.get("https://www.crossword.in/");	//Get the crossword url
+		System.out.println(driver.getTitle());	//To get the title
 		
-		Crossword_Excel data = new Crossword_Excel();
-		driver.findElement(By.linkText("Login")).click();
-		driver.findElement(By.id("user_session_email")).sendKeys(data.excel_username(1)); 
-		driver.findElement(By.id("user_session_password")).sendKeys(data.excel_password(1));
-		driver.findElement(By.xpath("//*[@id=\"user_session_submit\"]")).click();
+		Crossword_Excel data = new Crossword_Excel();	//Create an object for excel 
+		driver.findElement(By.linkText("Login")).click();	//Find Login Button
+		driver.findElement(By.id("user_session_email")).sendKeys(data.excel_username(1)); //Find username and send username by excel sheet
+		driver.findElement(By.id("user_session_password")).sendKeys(data.excel_password(1));	//Find password and send password by excel sheet
+		driver.findElement(By.xpath("//*[@id=\"user_session_submit\"]")).click();	//Click the submit Button
 	}
 	
 	//Scrolling the payment option page
 	public void scroll() throws InterruptedException
 	{
-		JavascriptExecutor js = (JavascriptExecutor) driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver; //Create an object for Scrolling option
 	    js.executeScript("window.scrollBy(0,1000)");
 	    
 	    Thread.sleep(1000);
-	    driver.findElement(payment).click();
+	    driver.findElement(payment).click();	//Find and click the payment 
 	    
 	    JavascriptExecutor js1 = (JavascriptExecutor) driver;
 	    js.executeScript("window.scrollBy(0,1000)");
 	    
-	    driver.findElement(takeMe).click();  
+	    driver.findElement(takeMe).click();  //Find and click the take me button
 	}
 	
 	
 	public void close_scroll() throws InterruptedException
 	{
 		Thread.sleep(3000);
-		driver.findElement(By.linkText("Logout")).click();
+		driver.findElement(By.linkText("Logout")).click();	//Find and click the logout button
 		Thread.sleep(2000);
-		driver.close();
+		driver.close(); //To close the browser
 	}
 }
